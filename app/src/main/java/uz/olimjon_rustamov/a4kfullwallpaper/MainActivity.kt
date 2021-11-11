7096f7070caf39f6607142ff8ccd0f00ee70a0e8
Olimjon <olimjonrustamov3477@gmail.com>
2021-11-11 19:18:41 +0500

    Project built

---
+++ app/src/main/java/uz/olimjon_rustamov/a4kfullwallpaper/MainActivity.kt
@@ -0,0 +1,11 @@
+package uz.olimjon_rustamov.a4kfullwallpaper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
